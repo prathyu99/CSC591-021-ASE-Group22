9a44febef4017464954718c9b87f64114473cea5
prathyu99 <prathyu13579@gmail.com>
2024-01-14 18:14:32 -0500

    add function in Symbol class

--- hw/w2/src/sym.py
+++ hw/w2/src/sym.py
@@ -7,4 +7,9 @@ class SYM:
         self.mode = None
         self.most = 0
 
-    
+    def add(self, x):
+        if x != "?":
+            self.n += 1
+            self.has[x] = 1 + self.has.get(x, 0)
+            if self.has[x] > self.most:
+                self.most, self.mode = self.has[x], x
