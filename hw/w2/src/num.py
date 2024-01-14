1b669f62c70a131c27c1fb0b175cd0a8547842e9
kothasrilakshmi <145411045+kothasrilakshmi@users.noreply.github.com>
2024-01-14 18:04:12 -0500

    NUM class initiated

---
+++ hw/w2/src/num.py
@@ -0,0 +1,21 @@
+class NUM:
+    def __init__(self, s=None, n=0):
+        self.txt = s or " "
+        self.at = n or 0
+        self.n = 0
+        self.mu = 0
+        self.m2 = 0
+        self.hi = -1E30
+        self.lo = 1E30
+        self.heaven = 0 if (s or "").find("−$") else 1
+
+    def add(self, x):
+        if x != "?":
+            self.n += 1
+            d = x - self.mu
+            self.mu += d / self.n
+            self.m2 += d * (x - self.mu)
+            self.lo = min(x, self.lo)
+            self.hi = max(x, self.hi)
+
+
