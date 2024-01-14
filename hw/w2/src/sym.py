a4c844a8d51d18f37478004cd082e6473c35424a
prathyu99 <prathyu13579@gmail.com>
2024-01-14 18:09:26 -0500

    initialized Symbol class

---
+++ hw/w2/src/sym.py
@@ -0,0 +1,10 @@
+class SYM:
+    def _init_(self, s=None, n=0):
+        self.txt = s or " "
+        self.at = n or 0
+        self.n = 0
+        self.has = {}
+        self.mode = None
+        self.most = 0
+
+    
